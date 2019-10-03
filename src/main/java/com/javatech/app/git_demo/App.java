76d85374fa21fbd47843c48504ab07f67412b41a
venkat <vijaypulla12@gmail.com>
2019-10-03 12:24:25 -0400

    second commit

--- src/main/java/com/javatech/app/git_demo/App.java
+++ src/main/java/com/javatech/app/git_demo/App.java
@@ -9,6 +9,11 @@ public class App {
 	public void addData(String data) {
 
 		System.out.println("data" + data);
+		
+		if(data == "valid"){
+			
+			System.out.println("data is valid");
+		}
 	}
 
 	public static void main(String[] args) {
