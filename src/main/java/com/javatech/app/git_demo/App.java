2ce00ef1d982f7fee43e1a29f2b11e310c6cedd9
venkat <vijaypulla12@gmail.com>
2019-10-03 12:16:16 -0400

    first commit

---
+++ src/main/java/com/javatech/app/git_demo/App.java
@@ -0,0 +1,17 @@
+package com.javatech.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public void addData(String data) {
+
+		System.out.println("data" + data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
